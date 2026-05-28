4f1a0682ef29ddfeaf56457f960bf8656c4df769
jaime <jaime251102@gmail.com>
2026-05-28 02:11:46 -0400

    modify jwt to use key from env

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,15 +2,19 @@ import fs from "fs"
 import path from "path"
 import jwt from "jsonwebtoken"
 
-const privateKey = fs.readFileSync(
-  path.join(__dirname, "../../keys/private.key"),
-  "utf8"
-)
+const getPrivateKey = (): string => {
+  if (process.env.JWT_PRIVATE_KEY) {
+    return process.env.JWT_PRIVATE_KEY.replace(/\\n/g, "\n")
+  }
+  return fs.readFileSync(path.join(__dirname, "../../keys/private.key"), "utf8")
+}
 
-const publicKey = fs.readFileSync(
-  path.join(__dirname, "../../keys/public.key"),
-  "utf8"
-)
+const getPublicKey = (): string => {
+  if (process.env.JWT_PUBLIC_KEY) {
+    return process.env.JWT_PUBLIC_KEY.replace(/\\n/g, "\n")
+  }
+  return fs.readFileSync(path.join(__dirname, "../../keys/public.key"), "utf8")
+}
 
 export interface JwtPayload {
   sub: string 
@@ -18,7 +22,7 @@ export interface JwtPayload {
 }
 
 export const generateAccessToken = (payload: JwtPayload) => {
-  return jwt.sign(payload, privateKey, {
+  return jwt.sign(payload, getPrivateKey(), {
     algorithm: "RS256",
     expiresIn: "15m"
   })
@@ -26,7 +30,7 @@ export const generateAccessToken = (payload: JwtPayload) => {
 
 export const verifyAccessToken = (token: string) => {
   try {
-    return jwt.verify(token, publicKey, {
+    return jwt.verify(token, getPublicKey(), {
       algorithms: ["RS256"]
     })
   } catch (error) {
